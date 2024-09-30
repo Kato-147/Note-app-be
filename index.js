727220d5085e4b1a45d1ffa64d268bf7e5ae3b35
Kato-147 <oroki147@gmail.com>
2024-10-01 01:25:00 +0700

    mongoDB connect

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+require('dotenv').config();
+
+const config = require('./config.json');
+const mongoose = require("mongoose"); // import mongoose library
+
+mongoose.connect(config.connectionString);
+
 const express = require("express"); // import express framework
-
 const cors = require("cors"); // import cors package
-
 const app = express(); // create express app instance and configure express middleware middle handlers with cors
+
+const jwt = require("jsonwebtoken");
+const {authenticateToken} = require("./utilities");
 
 app.use(express.json()); // use middleware to handle JSON data
 
-app.use(cors({ origin: "*" })); // config cors to accept any request from the any domain
 
+app.use(cors({ origin: "*" })); // config cors to accept any request from the any domain
 app.get("/", (req, res) => {
   res.json({ data: "hello world" });
 });
@@ -15,5 +23,7 @@ app.get("/", (req, res) => {
 app.listen(8000,() => {
     console.log('Server is running on port 8000');
   }); // port 8000
+
+
 
 module.exports = app;
