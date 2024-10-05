949fec3505cf7deea923c962e18f1bb1c95fb1cb
Kato-147 <oroki147@gmail.com>
2024-10-05 17:50:53 +0700

    edit & get all note api

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post("/login", async (req, res) => {
 //Add note
 app.post("/add-note", authenticateToken, async (req, res) => {
   const { title, content, tags } = req.body; // received data from body of request
-  const {user} = req.user;
+  const { user } = req.user;
 
   if (!title) {
     return res.status(400).json({ message: "Tiêu đe cần nhập." });
@@ -133,6 +133,60 @@ app.post("/add-note", authenticateToken, async (req, res) => {
     return res.json({ error: false, note, message: "Thêm ghi chú thành công" });
   } catch (error) {
     return res.status(500).json({ error: true, message: "Lỗi sever" });
+  }
+});
+
+//Edit note
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { title, content, tags, isPinned } = req.body; // received data from body of request
+  const { user } = req.user;
+
+  if (!title && !content && !tags) {
+    return res
+      .status(400)
+      .json({ message: "Vui lòng nhập cả tiêu đ��, nội dung và th��." });
+  }
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res
+        .status(404)
+        .json({
+          error: true,
+          message: "Ghi chú không tồn tại hoặc bạn không có quyền sửa.",
+        });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned) note.isPinned = isPinned;
+
+    await note.save();
+
+    return res.json({ error: false, note, message: "Sửa ghi chú thành công" });
+  } catch (error) {
+    return res.status(500).json({ error: true, message: "lỗi server" });
+  }
+});
+
+// get all notes
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+
+  try {
+    const notes = await Note.find({ userId: user._id }).sort({ createOn: -1 }); // get all notes of user with sort by  descending
+
+    return res.json({
+      error: false,
+      notes,
+      message: "Lấy danh sách ghi chú thành công",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Lỗi sever get all" });
   }
 });
 
